0f4754a76773ab6970c98df263f051f0170bd298
AryanPiash <aryanpiash420@gmail.com>
2022-05-29 23:35:27 +0600

    finalized commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port = process.env.PORT | 5000
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const app = express()
+const port = process.env.PORT | 5000
 
 app.use(cors())
 app.use(express.json())
